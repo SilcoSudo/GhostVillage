5354089b8dae92affa46a65633d3daf713cbbf14
Nguyễn Hồ Phi Hùng <kenvkl007@gmail.com>
2026-01-20 09:52:22 +0700

    Done các UI Cơ Bản của Lobby Room
    Tham gia phòng chung. Voice chat, Thông tin phòng

---
+++ GhostVillage Game/Assets/Game/Scripts/Core/DI/LobbySceneScope.cs
@@ -0,0 +1,21 @@
+using VContainer;
+using VContainer.Unity;
+using UnityEngine;
+using Game.Core.Lobby;
+
+namespace Game.Core.DI
+{
+    public class LobbySceneScope : LifetimeScope
+    {
+        [Header("Lobby Managers")]
+        [SerializeField] private LobbyUIManager _uiManager;
+        [SerializeField] private LobbyManager _lobbyManager;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            // Đăng ký trực tiếp tham chiếu đã kéo từ Inspector
+            if (_uiManager != null) builder.RegisterComponent(_uiManager);
+            if (_lobbyManager != null) builder.RegisterComponent(_lobbyManager);
+        }
+    }
+}
