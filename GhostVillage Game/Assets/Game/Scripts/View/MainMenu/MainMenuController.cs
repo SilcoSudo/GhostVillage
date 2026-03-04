b5d820c9bb2c4a50629c9e889c9e1d0fc50e8a72
iilweruri <nguyenhongthu7love@gmail.com>
2026-03-04 18:36:32 +0700

    Thêm Player và Shop Button

--- GhostVillage Game/Assets/Game/Scripts/View/MainMenu/MainMenuController.cs
+++ GhostVillage Game/Assets/Game/Scripts/View/MainMenu/MainMenuController.cs
@@ -15,6 +15,9 @@ namespace Game.UI.MainMenu
         [Header("UI References")]
         [SerializeField] private Button _lobbyListButton; // Nút vào xem danh sách
         [SerializeField] private Button _debugHostButton; // Nút test tạo nhanh phòng
+
+        [SerializeField] private Button _shopButton; // Nút vào cửa hàng
+
         [SerializeField] private TextMeshProUGUI _statusText; // Hiện trạng thái kết nối
 
         [Header("Profile Text")]
@@ -62,6 +65,7 @@ namespace Game.UI.MainMenu
                 _statusText.color = Color.yellow;
                 _lobbyListButton.interactable = false;
                 _debugHostButton.interactable = false;
+                _shopButton.interactable = false;
 
                 string playerName = _store.DisplayName.Value;
                 string token = _store.AuthToken.Value;
@@ -128,8 +132,8 @@ namespace Game.UI.MainMenu
             // Mở khóa các nút chức năng
             _lobbyListButton.interactable = true;
             _debugHostButton.interactable = true;
+            _shopButton.interactable = true;
         }
-
         // Gắn vào nút "Lobby List"
         public void OnLobbyListClick()
         {
@@ -142,6 +146,13 @@ namespace Game.UI.MainMenu
         {
             // Chuyển sang scene Profile
             _sceneLoader.LoadSceneAsync("ProfileScene");
+        }
+
+        // Gắn vào nút "Shop"
+        public void OpenShopScene() 
+        {
+            // Chuyển sang scene Shop
+            _sceneLoader.LoadSceneAsync("ShopScene");
         }
 
         // Gắn vào nút "Test Host" (Nút ảo để test)
